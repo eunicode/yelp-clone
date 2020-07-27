8f8ab458c511af62ca3f9609775eae3ebd56f7d4
Eunice Park <28845173+eunicode@users.noreply.github.com>
2020-07-27 15:48:36 -0700

    :art: Style `Business` component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,21 +1,21 @@
-// `Business` component will represent how a business will be formatted and styled.
-
-import React from "react";
+import React from 'react';
+import './Business.css';
 
 // Dummy business listing
 const business = {
   imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
+    'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
   rating: 4.5,
   reviewCount: 90,
 };
 
+// `Business` component will represent how a business will be formatted and styled.
 // `Business` class
 class Business extends React.Component {
   render() {
